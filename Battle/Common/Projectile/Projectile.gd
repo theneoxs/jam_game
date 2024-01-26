3415fb72a15d522c9933658220bef3f40d00149b
Alexander Kamenev <75051539+Nraj10@users.noreply.github.com>
2024-01-27 01:30:08 +0300

    Абстракция

---
+++ Battle/Common/Projectile/Projectile.gd
@@ -0,0 +1,30 @@
+extends Node2D
+var damage:int = 100
+var speed:int = 100
+var size:int = 100
+var projectileOwner:Object
+var isBomb:bool = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _create():
+	pass
+	
+func _move():
+	pass
+	
+func _hit():
+	pass
+	
+func _delete():
+	pass
+	
+func _explosion():
+	pass
---
+++ Battle/EnemyRelated/BaseEnemy.gd
@@ -0,0 +1,41 @@
+extends Node2D
+
+var hp:int = 100
+var speed:int = 100
+var attackDelay:int = 100
+var score:int = 100
+var damage:int = 100
+var item:Object 
+var baseSprite:Sprite2D
+var additionalSprite:Sprite2D
+var modEffect:Sprite2D
+var modification:Object
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _attack():
+	pass
+	
+func _death():
+	pass
+	
+func _dropAbbility():
+	pass
+	
+func _move():
+	pass
+	
+func _create():
+	pass
+	
+ #TODO _mutationOnTimer():
+	#pass
